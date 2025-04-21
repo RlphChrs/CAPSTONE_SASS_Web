43084875bcec765db116559e7637a19e69324acf
Ralph <ralphc.pilapil@gmail.com>
2025-04-21 21:30:31 +0800

    fixing error for deployment

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import LoginForm from '../components/LoginForm';
-import LoginImage from '../assets/loginAsset.png';
+import LoginImage from '../assets/LoginAsset.png';
 
 const Login = () => {
   return (
